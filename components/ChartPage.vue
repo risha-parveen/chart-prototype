821649d8cc788997b4457f96be984f6716051faf
rishaaa <rishaparveen12@gmail.com>
2022-11-08 19:12:43 +0530

    first commit

---
+++ components/ChartPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="chart-container">
+      <div class="left-container">
+        
+      </div>
+      <div class="right-container">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+
+  body {
+    margin: 0px;
+  }
+  .container {
+    height:100vh;
+    width:100vw;
+    background-color:rgb(225, 239, 225);
+  }
+
+  .chart-container {
+    width:100%;
+    height:100%;
+    display:flex;
+  }
+
+  .left-container {
+    width:50%;
+  }
+
+  .right-container {
+    width:50%;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <ChartPage/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
